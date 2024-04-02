a97a0c6b2741dd3c1d29f814a8db5eab51b0f444
Sergio_Guerra <guerrasergion@gmail.com>
2024-04-01 20:28:31 -0500

    Creacion proyecto redSolidaria
    Creacion archivos de configuración de SpringBoot

---
+++ redSolidaria/src/main/java/com/poli/redsolidaria/RedSolidariaApplication.java
@@ -0,0 +1,13 @@
+package com.poli.redsolidaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedSolidariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedSolidariaApplication.class, args);
+    }
+
+}
---
+++ redSolidaria/src/test/java/com/poli/redsolidaria/RedSolidariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poli.redsolidaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedSolidariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
