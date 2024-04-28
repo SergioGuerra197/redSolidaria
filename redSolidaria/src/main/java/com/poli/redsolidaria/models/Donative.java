86e121f7b1a69293ce7c085574f0a6506158f6d0
Sergio_Guerra <guerrasergion@gmail.com>
2024-04-28 18:56:32 -0500

    Creacion clase donativo

---
+++ redSolidaria/src/main/java/com/poli/redsolidaria/models/Donative.java
@@ -0,0 +1,36 @@
+package com.poli.redsolidaria.models;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "donatives")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Donative {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne //Muchos donativos pertenecen a un solo usuario
+    @JoinColumn(name = "user_id", nullable = false)
+    private User idUser;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "description", length = 300, nullable = false)
+    private String description;
+
+    @Column(name = "category", length = 100, nullable = false)
+    private String category;
+
+    @Column(name = "date", nullable = false)
+    private LocalDateTime donativeDate;
+
+    @Column(name = "state", nullable = false)
+    private String state;
+}
