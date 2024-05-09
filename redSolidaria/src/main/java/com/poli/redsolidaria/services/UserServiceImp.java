d5e7908d81b9b696ad4554397e959946f5e6469e
Salvel <samuel_alvarez82201@elpoli.edu.co>
2024-05-09 17:34:45 -0500

    se modificó el UserServiceImp y se finalizó el login

--- redSolidaria/src/main/java/com/poli/redsolidaria/services/UserServiceImp.java
+++ redSolidaria/src/main/java/com/poli/redsolidaria/services/UserServiceImp.java
@@ -3,24 +3,26 @@ package com.poli.redsolidaria.services;
 import com.poli.redsolidaria.models.User;
 import com.poli.redsolidaria.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
-import java.util.Collection;
+
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class UserServiceImp implements UserService{
 
     private final UserRepository userRepository;
+
 
     @Override
     public List<User> getAllUsers(){
