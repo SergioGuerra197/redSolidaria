9a7f1019c59ebbd600c628b1517e9c3ad3dc1207
Salvel <samuel_alvarez82201@elpoli.edu.co>
2024-05-09 16:00:31 -0500

    creación de SecurityConfig
    se añadieron dependencias y se modificaron los archivos: UserService y el UserServiceImp

--- redSolidaria/src/main/java/com/poli/redsolidaria/services/UserServiceImp.java
+++ redSolidaria/src/main/java/com/poli/redsolidaria/services/UserServiceImp.java
@@ -3,6 +3,8 @@ package com.poli.redsolidaria.services;
 import com.poli.redsolidaria.models.User;
 import com.poli.redsolidaria.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 
@@ -20,5 +22,10 @@ public class UserServiceImp implements UserService{
     @Override
     public void createUser(User user) {
         userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
     }
 }
