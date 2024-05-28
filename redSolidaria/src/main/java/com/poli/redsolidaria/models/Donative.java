a3b88b3111ad5d2ca79c0f29b548780f6b00d036
Sergio_Guerra <guerrasergion@gmail.com>
2024-05-28 14:59:09 -0500

    Update Donative.java

--- redSolidaria/src/main/java/com/poli/redsolidaria/models/Donative.java
+++ redSolidaria/src/main/java/com/poli/redsolidaria/models/Donative.java
@@ -22,11 +22,20 @@ public class Donative {
     @Column(name = "name", length = 100, nullable = false)
     private String name;
 
+    @Column(name = "type", length = 100, nullable = false)
+    private String type;
+
+    @Column(name = "amount", length = 100, nullable = false)
+    private Integer amount;
+
+    @Column(name = "image", length = 200 , nullable = false)
+    private String image;
+
     @Column(name = "description", length = 300, nullable = false)
     private String description;
 
-    @Column(name = "category", length = 100, nullable = false)
-    private String category;
+    @Column(name = "condition", length = 100, nullable = false)
+    private String condition;
 
     @Column(name = "date", nullable = false)
     private LocalDateTime donativeDate;
