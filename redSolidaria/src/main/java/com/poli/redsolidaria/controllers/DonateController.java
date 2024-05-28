8d6729e0133166ffd573eea64d58e2d0fdc1cbe4
Sergio_Guerra <guerrasergion@gmail.com>
2024-05-28 18:50:45 -0500

    Actualización controlador de donación

--- redSolidaria/src/main/java/com/poli/redsolidaria/controllers/DonateController.java
+++ redSolidaria/src/main/java/com/poli/redsolidaria/controllers/DonateController.java
@@ -1,9 +1,12 @@
 package com.poli.redsolidaria.controllers;
 
+import com.poli.redsolidaria.models.Donative;
 import org.springframework.ui.Model;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,8 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/donate")
 public class DonateController {
     @GetMapping
-    public String donate(Model model){
+    public String getForm(Model model){
+        model.addAttribute("donative", new Donative());
         model.addAttribute("mainTitle", "¡Empieza tu donacion!");
         return "/pages/donate";
+    }
+
+    @PostMapping
+    public String addDonative(@ModelAttribute("donative")Donative newDonative){
+        return "redirect:/donative?success";
     }
 }
