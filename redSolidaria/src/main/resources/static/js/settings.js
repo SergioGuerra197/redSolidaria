a3a8f7c28759b2856de0f05609b67df8d2f9b90f
Simon <simon_sanchez82192@elpoli.edu.co>
2024-06-04 23:02:53 -0500

    Manejo de validaciones en los campos de contraseñas en la configuracion del usuario - Validar contraseña actual (seguridad) - Verificar contraseñas iguales

--- redSolidaria/src/main/resources/static/js/settings.js
+++ redSolidaria/src/main/resources/static/js/settings.js
@@ -1,17 +1,50 @@
-var actualPass = document.getElementById("actual_password").value;
-var firstPass = document.getElementById("first_password").value;
-var secondPass = document.getElementById("password").value;
+const actualPass = document.getElementById("actual_password");
+const firstPass = document.getElementById("first_password");
+const secondPass = document.getElementById("password");
+const labels = document.getElementsByClassName("label__pass");
 
-// Event listener for input event on the current password field
-actualPass.addEventListener("input", () => {
-    if (actualPass.value.trim() === "") {
-        firstPass.addClass("disabled")
-        firstPass.disabled = true;
-        secondPass.addClass("disabled")
-        secondPass.disabled = true;
-    } else {
-        firstPass.disabled = false;
-        secondPass.disabled = false;
+function validatePassField() {
+    if (actualPass.value.trim() === "")
+        actualPass.removeAttribute("required");
+    /*if(!(firstPass === secondPass)){
+        secondPass.setCustomValidity("Las contraseñas no coinciden")
+//        document.getElementById("parrafoPrueba").display = "block";
+    }*/
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Function to enable or disable the new password fields
+    function toggleNewPasswordFields() {
+        if (actualPass.value.trim() === "") {
+            for (let i = 0; i < labels.length; i++) {
+                labels[i].classList.add("fixed");
+            }
+
+            firstPass.classList.add("disabled");
+            firstPass.disabled = true;
+            firstPass.value = "";
+
+            secondPass.classList.add("disabled");
+            secondPass.disabled = true;
+            secondPass.value = "";
+        } else {
+
+
+            for (let i = 0; i < labels.length; i++) {
+                labels[i].classList.remove("fixed");
+            }
+
+            firstPass.classList.remove("disabled");
+            firstPass.disabled = false;
+
+            secondPass.classList.remove("disabled");
+            secondPass.disabled = false;
+        }
     }
-});
 
+    // Event listener for input event on the current password field
+    actualPass.addEventListener("input", toggleNewPasswordFields);
+
+    // Initial call to set the correct state on page load
+    toggleNewPasswordFields();
+});
